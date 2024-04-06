c29a17c3145076df47566e60b78b00937ae7a962
Игорь Половко <bujhm2607@yandex.ru>
2024-04-07 05:24:16 +0700

    cos

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 PREC = 10**(-6)
+PI = 3.1415926535
+PI_2 = 6.28318530718
+
+def shift2Pi(x):
+    sh = abs(x)//PI_2
+    if x > 0:
+        x -= sh*PI_2
+    else:
+        x += sh*PI_2
+    return x
 
 def e(x):
     prev = 0
@@ -15,6 +25,7 @@ def e(x):
     return res
 
 def sin(x):
+    x = shift2Pi(x)
     prev = x - 1
     res = x
     n = 3
@@ -29,9 +40,29 @@ def sin(x):
         flag*=-1
     return res
 
+def cos(x):
+    x = shift2Pi(x)
+    prev = 0
+    res = 1
+    n = 2
+    flag = -1
+    while abs(res-prev) > PREC:
+        tmp = 1
+        for i in range(1, n+1):
+            tmp*=x/i
+        n += 2
+        prev = res
+        res += tmp*flag
+        flag*=-1
+    return res
+
+
+
 
 print(e(5))
 
-print(sin(0))
+print(sin(-94))
+
+print(cos(-150))
 
 
